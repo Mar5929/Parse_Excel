2e58efc668a2181e6b1edebedfaf817fe83f779d
Michael Rihm <31996264+Mar5929@users.noreply.github.com>
2018-02-11 19:54:02 -0500

    Add files via upload
    added formatting to the dictionary and included values

--- parse_excel.py
+++ parse_excel.py
@@ -2,33 +2,36 @@
 # in a way that Python can read
 import xlrd
 
-def main():
-    book = xlrd.open_workbook('SOWC 2014 Stat Tables_Table 9.xlsx')
 
-    sheet = book.sheet_by_name('Table 9 ')
+book = xlrd.open_workbook('SOWC 2014 Stat Tables_Table 9.xlsx')
 
-    # Outputs the range 0-302 and each number is an "index number" we can use.
+sheet = book.sheet_by_name('Table 9 ')
+
+# Outputs the range 0-302 and each number is an "index number" we can use.
+row_num = sheet.nrows
+
+# Dictionary
+data = {}
+
+# we no longer need a count statement to count our code because we know the data begins at line 14
+for i in range(14, row_num):
+    # Takes the list that is each row and saves it to the row variable. This makes our /
+    # code more readable.
+    row = sheet.row_values(i)
     row_num = sheet.nrows
+    country = row[1]    # for every iteration of i this prints the INDEX 1 of row, not the row 1
 
-    count = 0
-    data = {}
+    data[country] = {   # Adds the country as a key to the data dictionary in the format you want
+        'Child_labor': {
+            'total': [row[4],row[5]],
+            'male': [row[6],row[7]],
+            'female': [row[8],row[9]],
+        },
+        'child_marriage': {
+            'married_by_15': [row[10],row[11]],
+            'married_by_18': [row[12],row[13]],
+        }
+    }  
 
-    # For each object in the list (range of sheet.nrows)
-    # In python 2, range and xrange were different functions. In python 3 they are both under the same class.
-    for i in range(row_num):
-        if count < 15:
-            if i > 14:
-                # Takes the list that is each row and saves it to the row variable. This makes our /
-                # code more readable.
-                row = sheet.row_values(i)
-                row_num = sheet.nrows
+print (data['Afghanistan'])
 
-                # This is an index of the first row, it pulls out the country from each row we iterate over.
-                country = row[1]  # since the english name is in column B we need index 1
-
-                data[i, country] = {}  # Adds the country as a key to the data dictionary in the format you want
-                count += 1
-
-    print (data)
-
-main()
