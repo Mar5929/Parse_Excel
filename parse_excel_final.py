13f604da2724e268a07868618d092e526dd8d7fb
Michael Rihm <31996264+Mar5929@users.noreply.github.com>
2018-02-14 14:01:27 -0500

    Update parse_excel_final.py

--- parse_excel_final.py
+++ parse_excel_final.py
@@ -1,39 +1,47 @@
-"""
-    This is a script to parse child labor and child marriage data.
-    The excel file used in this script can be found here:
-        http://www.unicef.org/sowc2014/numbers/
-"""
-
+# This program will parse the excel file called "SOWC 2014 Stat Tables_Table 9.xlsx" /
+# in a way that Python can read
 import xlrd
-book = xlrd.open_workbook("SOWC 2014 Stat Tables_Table 9.xlsx")
-
-sheet = book.sheet_by_name("Table 9 ")
-
-row_num = sheet.nrows
-all_data = []
-data = {}
-for i in range(14, row_num):
-
-    # Start at 14th row, because that is where the countries begin
-    row = sheet.row_values(i)
-
-    country = row[1]
-
-    data[country] = {
-        'child_labor': {
-            'total': [row[4], row[5]],
-            'male': [row[6], row[7]],
-            'female': [row[8], row[9]],
-        },
-        'child_marriage': {
-            'married_by_15': [row[10], row[11]],
-            'married_by_18': [row[12], row[13]],
-        }
-    }
-
-    if country == "Zimbabwe":
-        break
-
-    all_data.append(data)
 import pprint
-pprint.pprint(all_data)
+
+book = xlrd.open_workbook('SOWC 2014 Stat Tables_Table 9.xlsx')
+
+sheet = book.sheet_by_name('Table 9 ')
+
+# Outputs the range 0-302 and each number is an "index number" we can use.
+row_num = sheet.nrows
+
+# Main list
+list = []
+# Dictionary
+data = {}
+
+# we no longer need a count statement to count our code because we know the data begins at line 14
+for i in range(14, row_num):
+    """
+    Takes the list that is each row and saves it to the row variable. This makes our /
+    code more readable.
+    """
+    row = sheet.row_values(i)
+    country = row[1]  # for every iteration of i this prints the INDEX 1 of row, not the row 1
+    if country == '':
+        break
+    elif country == 'Zimbabwe':
+        break
+    else:
+        data[country] = {   # Adds the country as a key to the data dictionary in the format you want
+            'Child_labor': {
+                'total': [row[4],row[5]],
+                'male': [row[6],row[7]],
+                'female': [row[8],row[9]],
+            },
+            'child_marriage': {
+                'married_by_15': [row[10],row[11]],
+                'married_by_18': [row[12],row[13]],
+            },
+        }
+        list.append(data)
+        data = {} # This gives you the ability to index the dictionary entries within the list
+
+pprint.pprint(list)
+
+
